80efe4b4ac9c3b9a4ec954b5eb5f421f83df7acc
ASubby <104353408+ASubby@users.noreply.github.com>
2022-05-04 11:26:33 +0100

    Added API showing current temperature

--- Src/app.js
+++ Src/app.js
@@ -1,1 +1,15 @@
-
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  humidityElement.innerHTML = response.data.main.humidity;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  cityElement.innerHTML = response.data.name;
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+}
+let apiKey = `749717f0f4a2e36ed817dec27de38882`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=England&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
