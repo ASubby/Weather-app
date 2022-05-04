33131c28a444a79b8cc2c0f35d5c53aa2a2634fa
ASubby <104353408+ASubby@users.noreply.github.com>
2022-05-04 19:01:30 +0100

    Added search engine , adjusted icon

--- Src/app.js
+++ Src/app.js
@@ -43,7 +43,20 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = `749717f0f4a2e36ed817dec27de38882`;
-let city = "England";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = `749717f0f4a2e36ed817dec27de38882`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
