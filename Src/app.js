0dd62809ad691207cf4f0364303b7a8e90971d2a
ASubby <104353408+ASubby@users.noreply.github.com>
2022-06-16 12:22:21 +0100

    Updated app.js

--- Src/app.js
+++ Src/app.js
@@ -68,7 +68,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = `749717f0f4a2e36ed817dec27de38882`;
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   console.log(apiUrl);
